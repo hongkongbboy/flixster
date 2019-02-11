1ab53f9f696f97699c45ece5ac205b881ac562db
kin <bigso28@bigso28s-MacBook-Pro.local>
2019-02-10 23:57:11 -0800

    first commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by kin on 2/10/19.
+//  Copyright © 2019 Kin. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
