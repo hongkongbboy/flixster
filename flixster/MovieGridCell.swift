403cb781be82902240867ce3d3a6e697a9fc5be7
kin <bigso28@bigso28s-macbook-pro.local>
2019-02-17 23:05:25 -0800

    Updated homework 2.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by kin on 2/17/19.
+//  Copyright © 2019 Kin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
